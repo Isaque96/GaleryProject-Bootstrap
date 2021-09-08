2970e85965e83e0926abff8421a0dda87048616a
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-08 18:25:41 -0300

    Fully Commited

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss';
+
+// Dependências
+import 'jquery';
+import 'bootstrap';
+
+// Meus Arquivos JS
+import './js/core/includes.js';
+import './js/plugins/cityButtons.js';
